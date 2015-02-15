81f24e4cf06c6c9846053c9e5c6a7187ac19c817
Vincenzo Favara <vin.favara@gmail.com>
2015-02-15 18:00:13 +0100

    Initial Commit

---
+++ TreasureHuntGoogle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TreasureHuntGoogle
+//
+//  Created by Vincenzo Favara on 15/02/15.
+//  Copyright (c) 2015 Vincenzo Favara (VinSoft). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
